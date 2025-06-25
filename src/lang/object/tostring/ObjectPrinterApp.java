62175283ec4975f448321905259e717faf2752ce
soyoungmoon <dalso51166868@gmail.com>
2025-06-25 17:01:57 +0900

    Revert "feat: immutable object"
    This reverts commit b90e3be709766f0b91871caf372c7bfde3f4ecc5.

--- src/lang/object/tostring/ObjectPrinterApp.java
+++
@@ -1,9 +0,0 @@
-package lang.object.tostring;
-
-public class ObjectPrinterApp {
-    public static void main(String[] args) {
-        ObjectPrinter.print("Hello World");
-        ObjectPrinter.print(new Dog("milky", 13));
-        ObjectPrinter.print(new Car("myCar"));
-    }
-}
