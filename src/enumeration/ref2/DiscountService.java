485ac5e76bdd04539c13769ebfce8d0d67d13d98
soyoungmoon <dalso51166868@gmail.com>
2025-07-16 14:47:54 +0900

    feat: enumeration

---
+++ src/enumeration/ref2/DiscountService.java
@@ -0,0 +1,10 @@
+
+package enumeration.ref2;
+
+
+public class DiscountService {
+
+    public int discount(Grade Grade, int price) {
+        return price * Grade.getDiscountPercent() / 100;
+    }
+}
---
+++ src/enumeration/ref3/EnumRefMain3_2.java
@@ -0,0 +1,18 @@
+package enumeration.ref3;
+
+
+public class EnumRefMain3_2 {
+
+    public static void main(String[] args) {
+
+        int price = 10000;
+
+        System.out.println("BASIC 등급의 할인 금액: " + Grade.BASIC.discount(price));
+        System.out.println("GOLD 등급의 할인 금액: " + Grade.BASIC.discount(price));
+        System.out.println("DIAMOND 등급의 할인 금액: " + Grade.BASIC.discount(price));
+    }
+}
+
+//BASIC 등급의 할인 금액: 1000
+//GOLD 등급의 할인 금액: 2000
+//DIAMOND 등급의 할인 금액: 3000
