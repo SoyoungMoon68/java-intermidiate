13ed9143c78ff87a2e2067b1d60befd54e92f794
soyoungmoon <dalso51166868@gmail.com>
2025-07-16 14:46:50 +0900

    feat: Clazz Class

---
+++ src/lang/clazz/ClassCreateMain.java
@@ -0,0 +1,17 @@
+package lang.clazz;
+
+public class ClassCreateMain {
+    public static void main(String[] args) throws Exception {
+        Class helloClass = Hello.class;
+        //Class helloClass = Class.forName("lang.clazz.Hello");
+
+        Hello hello = (Hello) helloClass.getDeclaredConstructor().newInstance();
+        String result = hello.hello();
+        System.out.println("hello = " + hello);
+        System.out.println("result = " + result);
+
+    }
+}
+//Result
+//hello = lang.clazz.Hello@5f184fc6
+//result = Hello
